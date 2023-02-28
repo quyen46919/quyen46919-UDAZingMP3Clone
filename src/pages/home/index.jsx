ba369b5740b6aad20fe62908de22c50722045a03
Trieu Dang <trieubd93710>
2023-02-28 20:40:48 +0700

    feat: create-partner-zing009
    feat: create-partner-zing009

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Sidebar from '../../components/Sidebar';
+import React, { useState } from 'react';
 import './styles.scss';
+import PartnerList from '../../components/PartnerList'
 
 Home.propTypes = {};
 
@@ -8,8 +10,17 @@ function Home(props) {
   return (
     <div className="home">
       <Sidebar />
+      <PartnerList />
     </div>
   );
+
+    // return <div className="home">
+    //     <PartnerList />
+    // </div>;
+
+    return <div className="home">
+        <PartnerList />
+    </div>;
 }
 
 export default Home;
