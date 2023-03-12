8ea75b881d38259979697414eddb8069492f283d
Duongbaxinh <duongbaxinh2021@gmail.com>
2023-03-12 12:57:12 +0700

    feat:create zing002

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Sidebar from '../../components/Sidebar';
-import Headers from "../Header";
+import React from "react";
+import Sidebar from "../../components/Sidebar";
+import Headers from "../../components/Header";
 import "./styles.scss";
 
 Home.propTypes = {};
