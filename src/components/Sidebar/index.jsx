c185efa8205e86915d4f92f6856b2297ba401c4e
hoangphuc91dtt <102318502+hoangphuc91dtt@users.noreply.github.com>
2024-04-08 02:37:45 +0700

    feat: create folder test/data zing001 (#14)
    feat: create zing006 page
    
    fix: comment
    
    feat: create zing006
    
    Co-authored-by: Luan-Thanhh <luanthanh0322@gmail.com>

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -5,29 +5,23 @@ import SidebarList from './SidebarList';
 import './styles.scss';
 
 function Sidebar(props) {
-  return (
-    <div className="sidebar">
-      <a
-        href="#"
-        className="sidebar__logo"
-      ></a>
-      <SidebarList />
-      <button className="sidebar__btn">
-        <img
-          src={Plus}
-          alt="Plus"
-          className="sidebar__btn--icon-plus"
-        />
-        <span>Tạo playlist mới</span>
-        <i className="sidebar__btn--mobile">
-          <img
-            src={ArrowLeft}
-            alt="ArrowLeft"
-          />
-        </i>
-      </button>
-    </div>
-  );
+    return (
+        <div className="sidebar">
+            <a href="#" className="sidebar__logo"></a>
+            <SidebarList />
+            <button className="sidebar__btn">
+                <img
+                    src={Plus}
+                    alt="Plus"
+                    className="sidebar__btn--icon-plus"
+                />
+                <span>Tạo playlist mới</span>
+                <i className="sidebar__btn--mobile">
+                    <img src={ArrowLeft} alt="ArrowLeft" />
+                </i>
+            </button>
+        </div>
+    );
 }
 
 export default Sidebar;
---
+++ src/test/data/NewMusicData.js
@@ -0,0 +1,34 @@
+const musics = [
+    {
+        id: 1,
+        image: 'https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/5/4/8/8/5488c2adbd0c2415102bd52d26030482.jpg',
+        title: 'V-Pop Tháng 2/2023',
+        singer: 'Đức Phúc, Hòa Minzy, MONO, JACK -J97',
+    },
+    {
+        id: 2,
+        image: 'https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/7/7/4/d/774da9673bc6e61614f3fd8273fb6236.jpg',
+
+        title: 'US-UK Tháng 3/2023',
+        singer: 'Pink, Adele, Sam Smith, Miley, Nicki Minaj ',
+    },
+    {
+        id: 3,
+        image: 'https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/6/6/e/5/66e575133b731456bd51bc689aa58b33.jpg',
+        title: 'K-Pop Tháng 2/2023',
+        singer: 'STAYC, XG, PURPLE, KIS ',
+    },
+    {
+        id: 4,
+        image: 'https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/0/1/8/5/01850f1ab036f1040bab4072320cbe0e.jpg',
+        title: 'C-Pop Tháng 2/2023',
+        singer: 'Yisa Yu, 林俊傑 JJ Lin, Lay ',
+    },
+    {
+        id: 5,
+        image: 'https://photo-resize-zmp3.zmdcdn.me/w320_r1x1_webp/cover/7/6/3/3/7633dfdba7afd8bee61731fd453facc5.jpg',
+        title: 'Indie Việt Tháng 2/2023',
+        singer: 'Pixel Neko, Clinton Kane, The Weeknd',
+    },
+];
+export default musics;
